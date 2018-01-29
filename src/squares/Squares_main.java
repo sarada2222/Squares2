ce8384cb536d10001659c71fd3a0f78fdf0a19c9
50616005 <50616005@50616005-pc>
2018-01-29 17:27:37 +0900

    squares3

--- src/squares/Squares_main.java
+++ src/squares/Squares_main.java
@@ -1,9 +1,12 @@
 package squares;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Arrays;
 
 public class Squares_main {
@@ -32,6 +35,8 @@ public class Squares_main {
 		Graph graph = new Graph(value);
 		graph.setBounds(5,5,655,455);
 		graph.setVisible(true);
+		
+		smain.writeCSV("csv/result.csv", value);
 	}
 
     public double[] getCSV1(String path, int n) { //CSVから1次元データ取り込み(n:データ数)
@@ -59,6 +64,48 @@ public class Squares_main {
         System.out.println("CSV取り込み = "+Arrays.toString(csvdata));
         return csvdata;
         //CSVから取り込みここまで
+    }
+    
+    public void writeCSV(String path, double[] data) {
+    	try {
+			FileWriter fw = new FileWriter(path, true); //true:追記、false:上書き
+			PrintWriter pw = new PrintWriter(new BufferedWriter(fw));
+			//内容を指定する
+			pw.print("id");
+			pw.print(",");
+			pw.print("objective");
+			pw.print(",");
+			pw.print("theta0");
+			pw.print(",");
+			pw.print("theta1");
+			pw.println();
+			for (int i = 0; i < data.length; i++) {
+				int a = i/3+1;
+				if(i%3 == 0) {
+					pw.print(a);
+					pw.print(",");
+					pw.print(data[i]);
+					pw.print(",");
+				}else if(i%3 == 1) {
+					pw.print(data[i]);
+					pw.print(",");
+				}else if(i%3 == 2){
+					pw.print(data[i]);
+					pw.print(",");
+					pw.println();
+				}//pw.print(",");
+				//pw.println();
+			}
+			pw.println();
+			//ファイルに書き出す
+			pw.close();
+			//終了メッセージを画面に出力する
+			System.out.println("出力完了 : " + path);
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+
     }
 
 
